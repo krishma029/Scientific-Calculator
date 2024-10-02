e2fdfe90c4eac962f11eba2b3735c25355b0d72b
krishma029 <ranparakrishma64@gmail.com>
2024-10-02 20:16:24 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -17,11 +17,12 @@ form.display.value = Math.sqrt(form.display.value);
 function ln(form) {   
 form.display.value = Math.log(form.display.value);  
 }   
-function exp(form)
- {   	form.display.value = Math.exp(form.display.value);  
-}   function deleteChar(input) 
-{   input.value = input.value.substring(0, 
-input.value.length - 1)  
+function exp(form){   
+	form.display.value = Math.exp(form.display.value);  
+}  
+function deleteChar(input) {  
+	input.value = input.value.substring(0, input.value.length - 1)  
+
 } 
 var val = 0.0; function percent(input) {   val = input.value;    input.value = input.value + "%";  
 }   
@@ -40,22 +41,25 @@ function compute(form) {
     //val = 0.0;  // } else      
  form.display.value = eval(form.display.value);  
   }  
-   function square(form)
- {   form.display.value = eval(form.display.value) * eval(form.display.value)  
-}   function checkNum(str)
- {    	for (var i = 0; i < str.length; i++) 
+   function square(form){   
+	   form.display.value = eval(form.display.value) * eval(form.display.value)  
+ }   
+function checkNum(str) {    
+	for (var i = 0; i < str.length; i++) 
 {    
-var ch = str.charAt(i);  
-  if (ch < "0" || ch > 
-"9") {    	  
- if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch  != "."  && ch != "(" && ch!= ")" && ch != "%")
+   var ch = str.charAt(i);  
+   if (ch < "0" || ch > "9")
+ {    	  
+   if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch  != "."  && ch != "(" && ch!= ")" && ch != "%")
 
-  	     	 {       	
+  	 {       	
 		alert("invalid entry!")        	
 		return false  
-  	   	  	}  
-  	   	}  
   	 }  
+  	   	
+      }  
+  	
+}  
   	 return true  
 }      
        
